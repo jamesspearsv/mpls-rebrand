008fc5632a1eeda5da501d539dc5ad332c0a19c7
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2025-02-12 17:10:32 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import ThemeSelector from './ui/ThemeSelector';
+
+export interface ThemeInterface {
+  primary: string;
+  secondary: string;
+  accent: string;
+}
+
+const ThemeOptions: ThemeInterface[] = [
+  { primary: '#0b1c48', secondary: '#710117', accent: '#e55b13' },
+  { primary: '#004aad', secondary: '#099b43', accent: '#ff8a01' },
+  { primary: '#1a4314', secondary: '#2c5e1a', accent: '#b2d2a4' },
+];
+
+const FontOptions = ['Mulish', 'Prompt'];
+
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [themeOption, setThemeOption] = useState(ThemeOptions[index]);
+  const [fontOption, setFontOption] = useState(FontOptions[0]);
+
+  useEffect(() => {
+    setThemeOption(ThemeOptions[index]);
+  }, [index]);
+
+  return (
+    <main style={{ fontFamily: fontOption }}>
+      <ThemeSelector
+        theme={themeOption}
+        themeOptions={ThemeOptions}
+        index={index}
+        setIndex={setIndex}
+        fontOptions={FontOptions}
+        fontOption={fontOption}
+        setfontOption={setFontOption}
+      />
+      <div>theme preview</div>
+    </main>
+  );
+}
