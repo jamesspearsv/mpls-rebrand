d5dfa75632dbbf31f688cd656a7763d778289025
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2025-02-18 16:26:33 -0500

    Begin styling design preview page

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
         setFont={setFont}
         fontOptions={FontOptions}
       />
-      <DesignPreview />
+      <DesignPreview colors={colorSet} />
     </main>
   );
 }
--- src/ui/DesignPreview.tsx
+++ src/ui/DesignPreview.tsx
@@ -1,44 +1,93 @@
+import { ColorSetInterface } from '../App';
 import styles from './DesignPreview.module.css';
 
-export default function DesignPreview() {
+export default function DesignPreview({
+  colors,
+}: {
+  colors: ColorSetInterface;
+}) {
   return (
     <div className={styles.container}>
       <div className={styles.preview}>
-        {/* <!-- Headings --> */}
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
-        <h6>Heading 6</h6>
-
+        <div className={styles.horizontal}>
+          {Object.keys(colors).map((color, index) => (
+            <div style={{ color: colors[color as keyof ColorSetInterface] }}>
+              {/* <!-- Headings --> */}
+              <h1>Heading 1</h1>
+              <h2>Heading 2</h2>
+              <h3>Heading 3</h3>
+              <h4>Heading 4</h4>
+              <h5>Heading 5</h5>
+              <h6>Heading 6</h6>
+            </div>
+          ))}
+        </div>
         {/* <!-- Body Text --> */}
-        <p>Body text - regular paragraph.</p>
-        <p>
-          <strong>Bold text example.</strong>
-        </p>
-        <p>
-          <em>Italic text example.</em>
-        </p>
-        <p>
-          <small>Small text example.</small>
-        </p>
-        <p>Underlined text example.</p>
-        <p>Muted text example.</p>
+        <div className={styles.horizontal}>
+          {Object.keys(colors).map((color, index) => (
+            <div style={{ color: colors[color as keyof ColorSetInterface] }}>
+              <p>Body text - regular paragraph.</p>
+              <p>
+                <strong>Bold text example.</strong>
+              </p>
+              <p>
+                <em>Italic text example.</em>
+              </p>
+              <p>
+                <small>Small text example.</small>
+              </p>
+              <p>Underlined text example.</p>
+              <p>Muted text example.</p>
+              <p>
+                Paragraph text example. Lorem ipsum odor amet, consectetuer
+                adipiscing elit. Ipsum eros massa fames, lacus mauris eros nisl.
+                Senectus erat nulla ut suspendisse accumsan quisque litora. Eros
+                euismod nunc sem orci facilisi. Mauris felis enim elementum
+                interdum nostra ex pharetra. Torquent nisl sit dapibus fames
+                odio justo. Montes mollis semper molestie gravida morbi! Posuere
+                platea euismod vel et ultricies ante consequat. Metus vehicula
+                praesent rhoncus cursus malesuada eleifend nec. Aptent placerat
+                inceptos nec, nisi nascetur molestie ligula tempus. Aenean
+                placerat platea primis mollis arcu tempus elementum sociosqu.
+                Sit amet facilisis nostra etiam a. At vehicula donec duis amet
+                augue maximus libero. Tempor odio aptent hac; leo natoque dui.
+              </p>
+            </div>
+          ))}
+        </div>
 
         {/* <!-- Buttons --> */}
-        <button>Primary Button</button>
-        <button disabled>Disabled Button</button>
-        <button>
-          <span role="img" aria-label="icon">
-            🔥
-          </span>{' '}
-          Icon Button
-        </button>
+        <div className={styles.horizontal}>
+          {Object.keys(colors).map((color) => (
+            <div
+              className={styles.vertical}
+              style={{ color: colors[color as keyof ColorSetInterface] }}
+            >
+              <button>Primary Button</button>
+              <button disabled>Disabled Button</button>
+              <button>
+                <span role="img" aria-label="icon">
+                  🔥
+                </span>{' '}
+                Icon Button
+              </button>
+            </div>
+          ))}
+        </div>
 
-        {/* <!-- Links --> */}
-        <a href="#">Default Link</a>
-        <a href="#">No Underline Link</a>
+        <div className={styles.horizontal}>
+          {Object.keys(colors).map((color) => (
+            <div
+              className={styles.vertical}
+              style={{ color: colors[color as keyof ColorSetInterface] }}
+            >
+              {/* <!-- Links --> */}
+
+              <a href="#">Default Link</a>
+              <a href="#">No Underline Link</a>
+            </div>
+          ))}
+        </div>
 
         {/* <!-- Lists --> */}
         <ul>
@@ -110,6 +159,20 @@ export default function DesignPreview() {
           <br />
           <button type="submit">Submit</button>
         </form>
+        {/* <!-- Accent Elements --> */}
+        <hr />
+        <div></div>
+        <span></span>
+        <svg width="50" height="50">
+          <circle
+            cx="25"
+            cy="25"
+            r="20"
+            stroke="black"
+            stroke-width="2"
+            fill="gray"
+          />
+        </svg>
       </div>
     </div>
   );
